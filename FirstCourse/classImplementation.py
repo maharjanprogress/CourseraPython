a1005dfbca7b7c0ed0fefba539cd86b492d23a20
maharjanprogress <progressmaharjan1@gmail.com>
2024-12-07 22:15:24 +0545

    Static and class method

--- FirstCourse/classImplementation.py
+++ FirstCourse/classImplementation.py
@@ -1,10 +1,27 @@
 class Character:
+    character_no = 0
     def __init__(self,health,damage,speed):
         self.health = health
         self.damage = damage
         self.speed = speed
+        
+        Character.character_no+=1
     def double_speed(self):
         self.speed *= 2
+    def speed(self):
+        return self.speed
+    
+    @classmethod
+    def set_character_no(cls,amt):
+        cls.character_no = int(amt)
+
+    @staticmethod
+    def ishigh(health):
+        if int(health)==100:
+            return True
+        else:
+            return False
+
 
 a=input("the thing:")
 a=int(a)
@@ -19,3 +36,17 @@ print(f"the speed was doubled for the warrior")
 
 print(f"Warrior speed: {warrior.speed}")
 print(f"Ninjaspeed: {ninja.speed}")
+
+#print with reference
+print(f"\nwith reference\n")
+
+print(f"Warrior speed: {Character.speed(warrior)}")
+print(f"Ninjaspeed: {Character.speed(ninja)}")
+print(f"\n\n\nToatal no of the character made: {Character.character_no}")
+Character.set_character_no(23)
+print(f"\n\n\nToatal no of the character made: {Character.character_no}")
+print(ninja.__dict__)
+
+#check if the health of the characters is full
+print(" Is the health of the character full???")
+print(Character.ishigh(ninja.health))
